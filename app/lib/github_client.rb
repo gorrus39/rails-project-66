43c4ddf10872e25fbc97b74ad123f856102d21b4
gorrus39 <gorrus100@gmail.com>
2024-04-17 14:15:23 +0200

    !

--- app/lib/github_client.rb
+++ app/lib/github_client.rb
@@ -45,20 +45,4 @@ class GithubClientStub
       clone_url: 'tovarish39/testmod'
     }]
   end
-
-  # def repos_collection(user)
-  # repos = repos_with_valid_lang(@client.repos, user)
-  # repos.map { |rep| [rep[:full_name], rep[:id]] }
-  # end
-
-  # private
-
-  # def repos_with_valid_lang(repos, user)
-  #   repos.filter do |rep|
-  #     next false unless rep[:language]
-
-  #     language = rep[:language].downcase.to_sym
-  #     user.repositories.new(language:).valid?
-  #   end
-  # end
 end
