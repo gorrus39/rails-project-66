3adcd3955692f910165173b3d3eb91358d1b2edd
gorrus39 <gorrus100@gmail.com>
2024-04-17 12:28:44 +0200

    !

--- app/lib/linter_handler.rb
+++ app/lib/linter_handler.rb
@@ -10,6 +10,7 @@ class LinterHandler
     if @language.ruby?
       rubocop_config_file = Rails.root.join('.rubocop.yml')
       result_rubocop = JSON.parse(`bundle exec rubocop --format json -c #{rubocop_config_file} #{dir_path}/**/*.rb`)
+
       format_after_rubocop(result_rubocop)
     elsif @language.javascript?
       output_file_path = "#{dir_path}/eslint_result.json"
