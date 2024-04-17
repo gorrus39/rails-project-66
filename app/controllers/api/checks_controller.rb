40f219033ec77aa944ab1d9e4c1d7592b5251385
gorrus39 <gorrus100@gmail.com>
2024-04-17 12:11:19 +0200

    !

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -7,7 +7,9 @@ module Api
     skip_before_action :verify_authenticity_token
     def from_webhook
       repository = Repository.find_by(full_name: request[:repository][:full_name])
+
       repsitory_check = repository.checks.create
+
       CheckingRepositoryJob.perform_later(repository, repository.user, repsitory_check)
       head :ok
     end
